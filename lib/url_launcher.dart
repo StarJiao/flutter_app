db579f197e7deb3905ef8553aa3692fb5c6dc513
jiaomingxing <jiaomingxing@cmcm.com>
2018-10-25 22:09:33 +0800

    add url launcher demo and gensture demo

---
+++ lib/url_launcher.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(
+    MaterialApp(title: "launch", theme: ThemeData.dark(), home: Launcher()),
+  );
+}
+
+class Launcher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: _launchURL,
+          child: new Text('Show Flutter homepage'),
+        ),
+      ),
+    );
+  }
+
+  _launchURL() async {
+//    const url = 'https://flutter.io';
+    const url = 'mailto:<jiaomingxing@conew.com>';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+}
